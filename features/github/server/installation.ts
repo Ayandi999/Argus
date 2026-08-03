de93f4fd1133a7892de1849189288cff42e04c99
Ayan-dip <ayandip6969@gmail.com>
2026-08-03 20:28:36 +0530

    chore : some comments to better understand the code

--- features/github/server/installation.ts
+++ features/github/server/installation.ts
@@ -24,7 +24,7 @@ function buildDisconnectedStatus(): GithubInstallationStatus {
   return { connected: false, accountLogin: null, installedAt: null };
 }
 
-//Checks if we alredy have any installation at all
+//Checks if user is alredy connected to github by looking at our DB table
 export async function getInstallationStatus(userId: string) {
   const installation = await prisma.githubInstallation.findUnique({
     where: {
@@ -45,11 +45,12 @@ export async function getInstallationStatus(userId: string) {
   };
 }
 
+//This is the most important one
 //This is go to DB and make a new installation instance
 export async function saveInstallation(userId: string, installationId: number) {
   const app = getGithubApp();
 
-  //This is just asking octokit for app data.
+  //This is just using octokit to ask github for app data using the installation id we got from redirect
   const { data } = await app.octokit.request(
     'GET /app/installations/{installation_id}',
     { installation_id: installationId }
@@ -76,10 +77,13 @@ export async function saveInstallation(userId: string, installationId: number) {
 
 // Disconnecting app
 export async function deleteInstallation(userId: string) {
+  //I need access to github app as well to delete from github
+  // app.octokit.rest.apps.deleteInstallation({intallationId})
   await prisma.githubInstallation.delete({ where: { userId } });
 }
 
-
+//This one is important for webhooks because if my app is installed then github sends all webhook request to my server directly
+//I need to know who the user assossiated with the app is to reditrect the user over there
 export async function getUserIdByInstallationId(installationId: number) {
   const installation = await prisma.githubInstallation.findFirst({
     where: { installationId },
@@ -93,6 +97,8 @@ export async function getUserIdByInstallationId(installationId: number) {
   return installation.userId;
 }
 
+//This one needed when i need to do some changes to github that's all
+//returns the installation id assossiated with the user.
 export async function getUserInstallationId(userId: string) {
   const installation = await prisma.githubInstallation.findUnique({
     where: { userId },
