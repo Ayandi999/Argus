2022f99a443db0ebeeb1421093586c851b4911f2
Ayan-dip <ayandip6969@gmail.com>
2026-08-06 10:06:16 +0530

    added some new features mostly infinit scrolling and UI update

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -3,11 +3,13 @@ import { inngest } from '@/features/inngest/client';
 
 import { processTask } from './sample-funtion';
 import { reviewPullRequest } from '@/features/reviews/server/review-pr-function';
+import { syncRepoCodebaseFunction } from '@/features/repo-sync/server/repo-sync-function';
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    processTask,
-    reviewPullRequest
+    processTask, 
+    reviewPullRequest, 
+    syncRepoCodebaseFunction
   ],
 });
